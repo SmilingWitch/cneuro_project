3015511a93a9ef8200650515d036ee610065bab0
SmilingWitch <danielitayero@gmail.com>
2023-12-28 10:20:46 -0500

    feat: add about/

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from '../page.module.css'
+
+import '../globals.css';
+import About from '@/components/About';
+
+export default function index() {
+  return (
+        <About/>
+    
+      )
+}
