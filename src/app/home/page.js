4fc6776465868ca996cb940cb56c033f4dcbaa29
SmilingWitch <danielitayero@gmail.com>
2023-12-28 10:21:28 -0500

    feat: add home/

---
+++ src/app/home/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from '../page.module.css'
+import Home from "@/components/Home"
+import '../globals.css';
+
+export default function index() {
+  return (
+        <Home/>
+    
+      )
+}
