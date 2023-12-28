2e5e893246e314aafbb4f4236c1e60d1df1f2ed3
SmilingWitch <danielitayero@gmail.com>
2023-12-28 17:48:35 -0500

    feat: add Aos animation to PartnersCard comp

--- src/components/ParticipantesCrad.jsx
+++ src/components/ParticipantesCrad.jsx
@@ -1,9 +1,20 @@
 import style from "../../public/assets/styles/PartersCard.module.css"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 export default function ParticipantesCard({name,level,occupation}){
 
+    useEffect(() => {
+        AOS.init({
+          duration:1500
+      });
+
+
+      }, []);
+
     return(
-        <div className={style.card}>
+        <div className={style.card} data-aos="fade-up">
             <div className={style.name}>
                 {name}
             </div>
