cb34ae9317a00d47e23f3d9c8af76f170df338c1
SmilingWitch <danielitayero@gmail.com>
2023-12-28 10:21:10 -0500

    feat: add team/

---
+++ src/app/team/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from '../page.module.css'
+
+import '../globals.css';
+import Team from '@/components/Team';
+
+export default function index() {
+  return (
+        <Team/>
+    
+      )
+}
