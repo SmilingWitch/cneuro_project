7816a77098cda23574afbc69ffcd7ff40337b55f
SmilingWitch <danielitayero@gmail.com>
2023-12-28 18:30:31 -0500

    feat: add active link to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,28 @@
 import Link from "next/link"
 import style from "../../public/assets/styles/Navbar.module.css"
-
+import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 
 export default function Navbar(){
+
+    const pathname = usePathname()
+    const router = useRouter();
+
+
     return(
         <div className={style.bar}>
             <ul>
                 <li>
-                   <Link href = "/home">Home</Link> 
+                   <Link href = "/home" className={`link ${pathname === '/home' ? `${style.active} `: ''}`}>Home</Link> 
                 </li>
                 <li>
-                   <Link href = "/about">About</Link> 
+                   <Link href = "/about" className={`link ${pathname === '/about' ? `${style.active} `: ''}`}>About</Link> 
                 </li>
                 <li>
-                    <Link href = "/team">Our team</Link>
+                    <Link href = "/team" className={`link ${pathname === '/team' ? `${style.active} `: ''}`}>Our team</Link>
                 </li>
                 <li>
-                    <Link href = "/">Project</Link>
+                    <Link href = "/" className={`link ${pathname === '/' ? `${style.active} `: ''}`}>Project</Link>
                 </li>
             </ul>
 
